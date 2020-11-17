a2cadc2d3a58b7f78faa72eea7acaaa37979617a
k-kudo-hub <tongariisaibou@gmail.com>
2020-11-17 15:32:53 +0900

    コードからURLを切り替える

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
     <h1>Home</h1>
+    <button @click="toUsers">Usersのページに遷移</button>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    toUsers(){
+      this.$router.push("users");
+    }
+  }
+}
+</script>
